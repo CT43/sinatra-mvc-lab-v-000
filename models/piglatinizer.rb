71a1232e7e44df2d8ee85434a12e7edb3ccff49f
Christian Thomas <christian.thomas.2894@gmail.com>
2018-08-08 21:12:20 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,19 @@
+require 'pry'
+
 class PigLatinizer
 
+  def igpay(text)
+    
 
+  end 
+
+  def vowel?(letter)
+    letter.match(/[AaEeIiOoUu]/)
+  end 
+
+  def latinize(word)
+    
+  end
 
 
 
